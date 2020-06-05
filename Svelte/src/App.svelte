5e862db058e07b2bfbf896f374728e72d8879352
unknown <mahammadanifr@gmail.com>
2020-06-05 17:43:35 +0530

    secound

---
+++ Svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+import Navbar from './Components/Navbar.svelte';
+import Home from './Components/Home.svelte';
+import Footer from './Components/Footer.svelte';
+import swal from 'sweetalert';
+</script>
+
+<Navbar />
+<Home />
---
+++ Svelte/src/Components/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+  } from 'sveltestrap';
+
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+  <script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+</script>
+
+<Navbar color="warning" dark>
+  <NavbarBrand href="/" class="mr-auto">Corona</NavbarBrand>
+  <NavbarToggler on:click={toggle} className="mr-2" />
+  <Collapse {isOpen} navbar>
+    <Nav navbar>
+      <NavItem>
+        <NavLink href="#components/">Components</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar>
